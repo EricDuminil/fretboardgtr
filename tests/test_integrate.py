533d6234d7700fb91308e614de2fa3d57fa91bf3
Eric Duminil <eric.duminil@gmail.com>
2023-02-10 22:16:34 +0100

    Autopep

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -12,140 +12,142 @@ HEADER = '<?xml version="1.0" encoding="utf-8" ?>\n'
 class IntegrateTest(unittest.TestCase):
 
     def test_scale_degree_name(self):
-        F=ScaleGtr(scale=["G","A","B","C","D","E","F#"],root="G")
-        F.customtuning(['E','A','D','G','B','E'])
+        F = ScaleGtr(scale=["G", "A", "B", "C", "D", "E", "F#"], root="G")
+        F.customtuning(['E', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/scale_degree_name.svg')
         F.draw()
 
-        with open(path / 'scale_degree_name.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'scale_degree_name.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_scale_no_color(self):
-        F=ScaleGtr(scale=["G","A","B","C","D","E","F#"],root="G")
-        F.customtuning(['E','A','D','G','B','E'])
-        F.theme(show_note_name=True,color_scale=False)
+        F = ScaleGtr(scale=["G", "A", "B", "C", "D", "E", "F#"], root="G")
+        F.customtuning(['E', 'A', 'D', 'G', 'B', 'E'])
+        F.theme(show_note_name=True, color_scale=False)
         F.pathname('img/scale_no_color.svg')
         F.draw()
 
-        with open(path / 'scale_no_color.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'scale_no_color.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_scale_on_seven_strings(self):
-        F=ScaleGtr(scale=["G","A","B","C","D","E","F#"], root="G")
-        F.customtuning(['B','E','A','D','G','B','E'])
-        F.theme(show_note_name=True,color_scale=False)
+        F = ScaleGtr(scale=["G", "A", "B", "C", "D", "E", "F#"], root="G")
+        F.customtuning(['B', 'E', 'A', 'D', 'G', 'B', 'E'])
+        F.theme(show_note_name=True, color_scale=False)
         F.pathname('img/scale_seven_strings.svg')
         F.draw()
 
-        with open(path / 'scale_seven_strings.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'scale_seven_strings.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_scale_note_name(self):
-        F=ScaleGtr(scale=["G","A","B","C","D","E","F#"],root="G")
-        F.customtuning(['E','A','D','G','B','E'])
+        F = ScaleGtr(scale=["G", "A", "B", "C", "D", "E", "F#"], root="G")
+        F.customtuning(['E', 'A', 'D', 'G', 'B', 'E'])
         F.theme(show_note_name=True)
-        F.theme(open_color_scale=True,string_size=3,default_theme=True)
+        F.theme(open_color_scale=True, string_size=3, default_theme=True)
         F.pathname('img/scale_note_name.svg')
         F.draw()
 
-        with open(path / 'scale_note_name.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'scale_note_name.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_TestScaleName(self):
-        F=ScaleGtr(ScaleFromName(root='F#',mode=Mode.IONIAN))
-        F.customtuning(['E','A','D','G','B','E'])
+        F = ScaleGtr(ScaleFromName(root='F#', mode=Mode.IONIAN))
+        F.customtuning(['E', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/TestScaleName.svg')
         F.theme(show_note_name=True)
         F.draw()
 
-        with open(path / 'TestScaleName.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'TestScaleName.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_TestScaleNameEnhar(self):
-        F=ScaleGtr(scale=['D#','F','G','G#','A#','C','D'], root='D#',enharmonic=True)
-        F.customtuning(['D','A','D','G','B','E'])
+        F = ScaleGtr(scale=['D#', 'F', 'G', 'G#', 'A#',
+                     'C', 'D'], root='D#', enharmonic=True)
+        F.customtuning(['D', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/TestScaleNameEnhar.svg')
         F.theme(show_note_name=True)
         F.draw()
 
-        with open(path / 'TestScaleNameEnhar.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'TestScaleNameEnhar.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_TestScaleNameEnhar24(self):
-        F=ScaleGtr(scale=['D#','F','G','G#','A#','C','D'], root='D#',enharmonic=True)
-        F.customtuning(['D','A','D','G','B','E'])
+        F = ScaleGtr(scale=['D#', 'F', 'G', 'G#', 'A#',
+                     'C', 'D'], root='D#', enharmonic=True)
+        F.customtuning(['D', 'A', 'D', 'G', 'B', 'E'])
         F.theme(last_fret=24)
         F.pathname('img/TestScaleNameEnhar24.svg')
         F.theme(show_note_name=True)
         F.draw()
 
-        with open(path / 'TestScaleNameEnhar24.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'TestScaleNameEnhar24.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_chord_degree_name(self):
-        F=ChordGtr(fingering=[0,3,2,0,1,0],root="C")
-        F.customtuning(['E','A','D','G','B','E'])
+        F = ChordGtr(fingering=[0, 3, 2, 0, 1, 0], root="C")
+        F.customtuning(['E', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/chord_degree_name.svg')
-        F.set_color(perfectfourth='rgb(0, 0, 0)',root='rgb(0, 0, 0)')
+        F.set_color(perfectfourth='rgb(0, 0, 0)', root='rgb(0, 0, 0)')
         F.draw()
 
-        with open(path / 'chord_degree_name.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'chord_degree_name.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_TestChordName(self):
-        F=ScaleGtr(ChordFromName(root='C',quality=Chord.MAJOR))
-        F.customtuning(['D','A','D','G','A','D'])
+        F = ScaleGtr(ChordFromName(root='C', quality=Chord.MAJOR))
+        F.customtuning(['D', 'A', 'D', 'G', 'A', 'D'])
         F.pathname('img/TestChordName.svg')
         F.draw()
 
-        with open(path / 'TestChordName.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'TestChordName.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_lefthandchord(self):
-        F=ChordGtr(fingering=[3,3,2,0,1,0],root="C",lefthand=True)
-        F.customtuning(['F','A','D','G','B','E'])
+        F = ChordGtr(fingering=[3, 3, 2, 0, 1, 0], root="C", lefthand=True)
+        F.customtuning(['F', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/lefthandchord.svg')
         F.draw()
 
-        with open(path / 'lefthandchord.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'lefthandchord.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_chord_name_long(self):
-        F=ChordGtr(fingering=[0,18,2,0,2,0],root="C")
-        F.customtuning(['F','A','D','G','B','E'])
+        F = ChordGtr(fingering=[0, 18, 2, 0, 2, 0], root="C")
+        F.customtuning(['F', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/chord_name_long.svg')
         F.theme(show_note_name=True)
         F.draw()
 
-        with open(path / 'chord_name_long.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
+        with open(path / 'chord_name_long.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_chord_name_background(self):
-        F=ChordGtr(fingering=[0,3,2,0,1,0],root="C")
-        F.customtuning(['F','A','D','G','B','E'])
+        F = ChordGtr(fingering=[0, 3, 2, 0, 1, 0], root="C")
+        F.customtuning(['F', 'A', 'D', 'G', 'B', 'E'])
         F.pathname('img/chord_name_background.svg')
-        F.theme(show_note_name=True,background_color='rgb(70,70,70)')
+        F.theme(show_note_name=True, background_color='rgb(70,70,70)')
         F.draw()
 
-        with open(path / 'chord_name_background.svg','r') as f:
-            file=f.read()
-            self.assertEqual(F.dwg.tostring(),file.split(HEADER)[1])
-
+        with open(path / 'chord_name_background.svg', 'r') as f:
+            file = f.read()
+            self.assertEqual(F.dwg.tostring(), file.split(HEADER)[1])
 
     def test_save_diagram_in_different_formats(self):
-        extensions = ['png', 'pdf', 'bmp', 'gif', 'svg'] # NOTE: SVG should be the last one
-        F=ScaleGtr(root='A')
+        # NOTE: SVG should be the last one
+        extensions = ['png', 'pdf', 'bmp', 'gif', 'svg']
+        F = ScaleGtr(root='A')
         with tempfile.TemporaryDirectory() as tmpdirname:
             for ext in extensions:
                 with self.subTest(ext=ext):
@@ -153,7 +155,7 @@ class IntegrateTest(unittest.TestCase):
                     F.pathname(out)
                     self.assertFalse(out.exists(),
                                      f"{out} should not have been created yet")
-                    F.save() # NOTE: Extension has been specified in pathname
+                    F.save()  # NOTE: Extension has been specified in pathname
                     self.assertTrue(out.exists(),
                                     f"{out} should have been written.")
                     if ext != 'svg':
@@ -172,5 +174,5 @@ class IntegrateTest(unittest.TestCase):
                                     f"{out2} should not be empty.")
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     unittest.main()
